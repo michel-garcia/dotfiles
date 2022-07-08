ca0178559985b5b0fa6d411dc39fdde03cbe9ac8
Michel Garcia <michelgarxia@gmail.com>
2022-07-07 20:34:21 -0400

    add basic leader keybindings

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -1,2 +1,7 @@
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
+
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>wv", ":vsplit")
+vim.keymap.set("n", "<leader>wh", ":split")
