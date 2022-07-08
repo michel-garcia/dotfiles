fb1511cf32278b1675f603d1aa682e8a97b71175
Michel Garcia <michelgarxia@gmail.com>
2022-07-07 20:34:58 -0400

    change packer window borders

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -41,7 +41,7 @@ packer.startup({
         display = {
             open_fn = function ()
                 return require("packer.util").float({
-                    border = "rounded"
+                    border = "single"
                 })
             end
         }
