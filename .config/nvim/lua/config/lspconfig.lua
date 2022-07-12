525db7c0ab245bc5f0ec083798f24c90d46ec344
Michel Garcia <michelgarxia@gmail.com>
2022-07-11 23:15:44 -0400

    add lsp keybindings and on attach notification

--- .config/nvim/lua/config/lspconfig.lua
+++ .config/nvim/lua/config/lspconfig.lua
@@ -1,18 +1,42 @@
+function bind_buffer_mappings(buffer)
+    local opts = {
+        buffer = buffer
+    }
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<space>f", vim.lsp.buf.formatting, opts)
+end
+
 function get_server_configuration (server_name)
     local completion = require("cmp_nvim_lsp")
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     completion.update_capabilities(capabilities)
     local configuration = {
         capabilities = capabilities,
-        on_attach = function (client)
+        on_attach = function (client, buffer)
+            bind_buffer_mappings(buffer)
+            vim.notify("Language server attached!", "info", {
+                title = server_name
+            })
             local illuminate = require("illuminate")
             illuminate.on_attach(client)
         end
     }
-    local filename = "lsp." .. server_name
-    local found, settings = pcall(require, filename)
+    local filename = string.format("lsp.%s", server_name)
+    local found, custom_configuration = pcall(require, filename)
     if found then
-        configuration = vim.tbl_deep_extend("force", settings, configuration)
+        configuration = vim.tbl_deep_extend(
+            "force",
+            custom_configuration,
+            configuration
+        )
     end
     return configuration
 end
@@ -47,8 +71,12 @@ return {
             signs = false,
             underline = true,
             update_in_insert = true,
-            virtual_text = true
+            virtual_text = false
         })
+        vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+        vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+        vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
     end
 }
 
