755776bed6c228e57a05e836d995c27ca95d4909
michel-garcia <michelgarxia@gmail.com>
2022-08-10 23:37:48 -0400

    show git ignored files

--- nvim/.config/nvim/lua/config/tree.lua
+++ nvim/.config/nvim/lua/config/tree.lua
@@ -13,8 +13,23 @@ return {
                     }
                 }
             },
+            git = {
+                enable = true,
+                ignore = false
+            },
             hijack_directories = {
                 enable = true
+            },
+            renderer = {
+                highlight_git = true,
+                icons = {
+                    show = {
+                        git = false
+                    }
+                }
+            },
+            view = {
+                hide_root_folder = true
             }
         })
     end
