9a91f35be43523dbb4588418d676bf5e5d88c975
michel-garcia <michelgarxia@gmail.com>
2023-02-16 00:08:41 -0500

    wrapped winbar highlights in an autocmd

--- nvim/.config/nvim/lua/user/plugins/lualine.lua
+++ nvim/.config/nvim/lua/user/plugins/lualine.lua
@@ -119,8 +119,15 @@ return {
                 }
             }
         })
-        vim.cmd("hi lualine_c_inactive none")
-        vim.cmd("hi link lualine_c_inactive lualine_c_normal")
+        vim.api.nvim_create_autocmd("ColorScheme", {
+            callback = function ()
+                vim.cmd("hi lualine_c_inactive none")
+                vim.cmd("hi link lualine_c_inactive lualine_c_normal")
+            end,
+            group = vim.api.nvim_create_augroup("LualineHighlights", {
+                clear = true
+            })
+        })
     end
 }
 
