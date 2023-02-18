6f7c1a0f36edd03b4eb5b2c8007bb7a684a14026
michel-garcia <michelgarxia@gmail.com>
2023-02-17 23:37:07 -0500

    add persistence fuzzy session picker

--- nvim/.config/nvim/lua/user/plugins/persistence.lua
+++ nvim/.config/nvim/lua/user/plugins/persistence.lua
@@ -3,12 +3,40 @@ return {
     config = function ()
         local persistence = require("persistence")
         persistence.setup()
-        vim.api.nvim_create_user_command("SessionLoad", function ()
+        vim.api.nvim_create_user_command("PersistenceLoad", function ()
             persistence.load()
         end, {})
-        vim.api.nvim_create_user_command("SessionLoadLast", function ()
+        vim.api.nvim_create_user_command("PersistenceLoadLast", function ()
             persistence.load({
                 last = true
+            })
+        end, {})
+        vim.api.nvim_create_user_command("PersistenceList", function ()
+            local paths = {}
+            local fzf = require("fzf-lua")
+            fzf.fzf_exec(function (callback)
+                    paths = {}
+                    for _, path in ipairs(persistence.list()) do
+                        local filename = vim.fn.fnamemodify(path, ":t:r")
+                        local name = filename:gsub("%%", "/")
+                        paths[name] = path
+                        callback(name)
+                    end
+                    callback()
+                end, {
+                actions = {
+                    ["default"] = function (selected)
+                        local path = vim.fn.fnameescape(paths[selected[1]])
+                        if vim.fn.exists(path) then
+                            vim.cmd.source(path)
+                        end
+                    end,
+                    ["ctrl-x"] = function (selected)
+                        local path = paths[selected[1]]
+                        vim.fn.delete(path)
+                        fzf.resume()
+                    end
+                }
             })
         end, {})
     end
