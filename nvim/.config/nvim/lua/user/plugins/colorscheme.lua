6af94bff2837e41f5e139bcf2df59bb9251c03f7
michel-garcia <michelgarxia@gmail.com>
2023-03-24 22:10:01 -0400

    feat(nvim, kitty): add diagnostic undercurls

--- nvim/.config/nvim/lua/user/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/user/plugins/colorscheme.lua
@@ -1,7 +1,23 @@
 return {
     "luisiacc/gruvbox-baby",
     config = function ()
-        vim.cmd("colorscheme gruvbox-baby")
+        vim.cmd.colorscheme("gruvbox-baby")
+        vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", {
+            sp = "red",
+            undercurl = true
+        })
+        vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", {
+            sp = "orange",
+            undercurl = true
+        })
+        vim.api.nvim_set_hl(0, "DiagnosticUnderlineInfo", {
+            sp = "lightblue",
+            undercurl = true
+        })
+        vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", {
+            sp = "lightgray",
+            undercurl = true
+        })
     end
 }
 
