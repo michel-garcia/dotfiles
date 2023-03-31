2260895365de48d9adbe78f2a37c4ae7c0216d27
Michel Garcia <michelgarxia@gmail.com>
2023-03-30 22:29:50 -0400

    feat(nvim): remove colorscheme background

--- nvim/.config/nvim/lua/user/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/user/plugins/colorscheme.lua
@@ -1,6 +1,7 @@
 return {
     "luisiacc/gruvbox-baby",
     config = function ()
+        vim.g.gruvbox_baby_transparent_mode = true
         vim.cmd.colorscheme("gruvbox-baby")
         vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", {
             sp = "red",
