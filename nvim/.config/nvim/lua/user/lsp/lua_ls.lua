d2bb5c3fa698bf22b260b56e853028cb667503d6
Michel Garcia <michelgarxia@gmail.com>
2023-05-16 10:10:18 -0400

    feat: isolate awesome and neovim lua configurations

--- nvim/.config/nvim/lua/user/lsp/lua_ls.lua
+++ nvim/.config/nvim/lua/user/lsp/lua_ls.lua
@@ -1,21 +1,8 @@
 return {
     settings = {
         Lua = {
-            diagnostics = {
-                globals = {
-                    "awesome",
-                    "beautiful",
-                    "client",
-                    "root",
-                    "screen",
-                    "vim"
-                }
-            },
-            workspace = {
-                library = {
-                    vim.api.nvim_get_runtime_file("lua", true),
-                    "/usr/share/awesome/lib"
-                }
+            telemetry = {
+                enable = false
             }
         }
     }
