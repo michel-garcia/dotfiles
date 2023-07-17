9938261cb7c345cb4313cd0304daa7d0a37cdb2f
Michel Garcia <michelgarxia@gmail.com>
2023-07-16 20:25:47 -0400

    feat(nvim): add lir file explorer plugin

--- nvim/.config/nvim/lua/user/plugins/fzf.lua
+++ nvim/.config/nvim/lua/user/plugins/fzf.lua
@@ -1,99 +1,78 @@
 return {
-    {
-        "ibhagwan/fzf-lua",
-        config = function ()
-            local fzf = require("fzf-lua")
-            fzf.setup({
-                buffers = {
-                    fzf_opts = {
-                        ["--info"] = false
-                    },
-                    no_header = true
-                },
-                files = {
-                    cwd_prompt = false,
-                    file_icons = false,
-                    fzf_opts = {
-                        ["--info"] = false
-                    },
-                    git_icons = false,
-                    path_shorten = true,
-                    prompt = "Files> "
-                },
+    "ibhagwan/fzf-lua",
+    config = function ()
+        local fzf = require("fzf-lua")
+        fzf.setup({
+            buffers = {
                 fzf_opts = {
-                    ["--algo"] = "v1",
-                    ["--color"] = table.concat({
-                        "bg+:6",
-                        "fg+:0",
-                        "hl:-1",
-                        "hl+:0",
-                        "gutter:-1",
-                        "pointer:0",
-                        "prompt:6"
-                    }, ","),
-                    ["--no-info"] = "",
-                    ["--no-scrollbar"] = "",
-                    ["--scroll-off"] = 5
+                    ["--info"] = false
                 },
-                grep = {
-                    fzf_opts = {
-                        ["--info"] = false
-                    },
-                    no_header = true
+                no_header = true
+            },
+            files = {
+                cwd_prompt = false,
+                file_icons = false,
+                fzf_opts = {
+                    ["--info"] = false
                 },
-                tabs = {
-                    fzf_opts = {
-                        ["--info"] = false
-                    },
-                    no_header = true
+                git_icons = false,
+                path_shorten = true,
+                prompt = "Files> "
+            },
+            fzf_opts = {
+                ["--algo"] = "v1",
+                ["--color"] = table.concat({
+                    "bg+:6",
+                    "fg+:0",
+                    "hl:-1",
+                    "hl+:0",
+                    "gutter:-1",
+                    "pointer:0",
+                    "prompt:6"
+                }, ","),
+                ["--no-info"] = "",
+                ["--no-scrollbar"] = "",
+                ["--scroll-off"] = 5
+            },
+            grep = {
+                fzf_opts = {
+                    ["--info"] = false
                 },
-                winopts = {
-                    hl = {
-                        border = "FloatBorder",
-                        normal = "FloatBorder"
-                    },
-                    preview = {
-                        hidden = "hidden"
-                    }
+                no_header = true
+            },
+            tabs = {
+                fzf_opts = {
+                    ["--info"] = false
                 },
-                winopts_fn = function ()
-                    return {
-                        border = vim.g.borders.frame,
-                        col = .5,
-                        height = 15,
-                        row = .5,
-                        width = 80
-                    }
-                end
-            })
-            local opts = {
-                noremap = true,
-                silent = true
-            }
-            vim.keymap.set("n", "<leader>fb", fzf.buffers, opts)
-            vim.keymap.set("n", "<leader>ff", fzf.files, opts)
-            vim.keymap.set("n", "<leader>sh", fzf.help_tags, opts)
-            vim.keymap.set("n", "<leader>fg", fzf.live_grep, opts)
-        end
-    },
-    {
-        "michel-garcia/fzf-lua-file-browser.nvim",
-        dev = true,
-        dependencies = {
-            "ibhagwan/fzf-lua"
-        },
-        config = function ()
-            local file_browser = require("fzf-lua-file-browser")
-            file_browser.setup({
-                hidden = true
-            })
-            local opts = {
-                noremap = true,
-                silent = true
-            }
-            local fzf = require("fzf-lua")
-            vim.keymap.set("n", "<leader>fe", fzf.file_browser, opts)
-        end
-    }
+                no_header = true
+            },
+            winopts = {
+                hl = {
+                    border = "FloatBorder",
+                    normal = "FloatBorder"
+                },
+                preview = {
+                    hidden = "hidden"
+                }
+            },
+            winopts_fn = function ()
+                return {
+                    border = vim.g.borders.frame,
+                    col = .5,
+                    height = 15,
+                    row = .5,
+                    width = 80
+                }
+            end
+        })
+        local opts = {
+            noremap = true,
+            silent = true
+        }
+        vim.keymap.set("n", "<leader>fb", fzf.buffers, opts)
+        vim.keymap.set("n", "<leader>ff", fzf.files, opts)
+        vim.keymap.set("n", "<leader>sh", fzf.help_tags, opts)
+        vim.keymap.set("n", "<leader>fg", fzf.live_grep, opts)
+    end
 }
 
---
+++ nvim/.config/nvim/lua/user/plugins/lir.lua
@@ -0,0 +1,48 @@
+return {
+    "tamago324/lir.nvim",
+    config = function ()
+        local lir = require("lir")
+        local actions = require("lir.actions")
+        lir.setup({
+            float = {
+                win_opts = function ()
+                    return {
+                        border = vim.g.borders.frame,
+                        col = (vim.o.columns - 80) / 2,
+                        height = 15,
+                        row = (vim.o.lines - 15) / 2,
+                        width = 80
+                    }
+                end
+            },
+            mappings = {
+                ["l"] = actions.edit,
+                ["h"] = actions.up,
+                ["<C-s>"] = actions.split,
+                ["<C-v>"] = actions.vsplit,
+                ["<C-t>"] = actions.tabedit,
+                ["<C-c>"] = actions.quit,
+                ["<Esc>"] = actions.quit,
+                ["q"] = actions.quit,
+                ["%"] = actions.touch,
+                ["d"] = actions.mkdir,
+                ["x"] = actions.cut,
+                ["y"] = actions.copy,
+                ["p"] = actions.paste,
+                ["R"] = actions.rename,
+                ["D"] = actions.delete
+            },
+            show_hidden_files = true
+        })
+        vim.api.nvim_set_hl(0, "LirFloatNormal", {
+            link = "NormalFloat"
+        })
+        vim.api.nvim_set_hl(0, "LirFloatCurdirWindowNormal", {
+            link = "NormalFloat"
+        })
+        vim.keymap.set("n", "<leader>e", function ()
+            local float = require("lir.float")
+            float.init()
+        end, { noremap = true, silent = true })
+    end
+}
