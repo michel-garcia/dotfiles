0616c494be8bd1ffc93abac8aad6ba88cc755e5f
Michel Garcia <michelgarxia@gmail.com>
2023-09-10 22:04:45 -0400

    feat(awesome): add seconds to clock widget

--- awesome/.config/awesome/user/widgets/clock.lua
+++ awesome/.config/awesome/user/widgets/clock.lua
@@ -4,7 +4,8 @@ local Clock = function ()
     local date = wibox.widget.textclock()
     date.format = "%a, %b %-e"
     local time = wibox.widget.textclock()
-    time.format = "%H:%M"
+    time.format = "%H:%M:%S"
+    time.refresh = 1
     local layout = wibox.layout.fixed.horizontal(date, time)
     layout.spacing = 8
     return wibox.container.margin(layout, 4, 4)
