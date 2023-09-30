847c90fd40c06cafd559af735d825f90710222e9
Michel Garcia <michelgarxia@gmail.com>
2023-09-30 12:58:56 -0400

    feat(awesome): tweak theme colours and add useless gap

--- awesome/.config/awesome/user/theme/init.lua
+++ awesome/.config/awesome/user/theme/init.lua
@@ -4,17 +4,18 @@ local naughty = require("naughty")
 local Gtk = require("user.modules.Gtk")
 
 beautiful.init({
-    bg_normal = "#000000",
+    bg_normal = "#282828",
     border_width = 2,
-    border_normal = "#000000",
-    border_focus = "#c78f23",
+    border_normal = "#2b2b2b",
+    border_focus = "#8c7868",
     font = "Ubuntu 10",
     systray_icon_spacing = 6,
     taglist_fg_empty = "#665c54",
-    taglist_bg_focus = "#c78f23",
+    taglist_bg_focus = "#89b482",
     taglist_fg_focus = "#000000",
     taglist_fg_occupied = "#e7d7ad",
-    taglist_spacing = 4
+    taglist_spacing = 4,
+    useless_gap = 4
 })
 
 naughty.config.defaults.border_width = 1
