8bec30ac91e89102b7d9dc7846e9f71537cacd36
Michel Garcia <michelgarxia@gmail.com>
2024-02-22 01:36:01 -0500

    feat: add project plugin

---
+++ nvim/.config/nvim/lua/user/plugins/project.lua
@@ -0,0 +1,15 @@
+return {
+    "ahmedkhalf/project.nvim",
+    main = "project_nvim",
+    opts = {
+        patterns = {
+            ".git",
+            "Makefile",
+            "composer.json",
+            "meson.build",
+            "node_modules",
+            "package.json",
+            "vendor"
+        }
+    }
+}
