68cd46031fb30579e63ef1f4a47640f8f34f4b6e
Michel Garcia <michelgarxia@gmail.com>
2024-03-05 20:51:15 -0500

    feat: add formatters to conform plugin

--- nvim/.config/nvim/lua/user/plugins/conform.lua
+++ nvim/.config/nvim/lua/user/plugins/conform.lua
@@ -2,11 +2,17 @@ return {
     "stevearc/conform.nvim",
     config = function()
         local conform = require("conform")
-        conform.setup()
+        conform.setup({
+            formatters_by_ft = {
+                javascript = { "prettier" },
+                javascriptreact = { "prettier" },
+                php = { "pretty-php" }
+            }
+        })
         vim.keymap.set("n", "<A-f>", function()
             conform.format({
                 async = true,
-                lsp_fallback = true
+                lsp_fallback = false
             })
         end, {
             noremap = true,
