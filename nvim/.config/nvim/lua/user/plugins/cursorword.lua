6ab95343c895679e17f7a0d16777a96873ac6e66
Michel Garcia <michelgarxia@gmail.com>
2024-03-14 21:05:52 -0400

    feat(nvim): remove custom highlight from cursorword

--- nvim/.config/nvim/lua/user/plugins/cursorword.lua
+++ nvim/.config/nvim/lua/user/plugins/cursorword.lua
@@ -2,12 +2,5 @@ return {
     "echasnovski/mini.cursorword",
     opts = {
         delay = 0
-    },
-    config = function (opts)
-        local cursorword = require("mini.cursorword")
-        cursorword.setup(opts)
-        vim.api.nvim_set_hl(0, "MiniCursorword", {
-            link = "MatchParen"
-        })
-    end
+    }
 }
