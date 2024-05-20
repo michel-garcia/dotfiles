82f803721269de0d45ef27ecdd6210a5caede943
Michel Garcia <michelgarxia@gmail.com>
2024-05-20 16:40:20 -0400

    feat(nvim): fix treesitter query injections for php

--- nvim/.config/nvim/queries/php/injections.scm
+++ nvim/.config/nvim/queries/php/injections.scm
@@ -1,5 +1,19 @@
 ; extends
 
 (encapsed_string
-    (string_value) @sql
-    (#match? @sql "^\s*(ALTER|CREATE|DELETE|DROP|EXPLAIN|INSERT|REPLACE|SELECT|UPDATE)"))
+    (string_content) @injection.content
+    (#match?
+        @injection.content
+        "^\s*(ALTER|CREATE|DELETE|DROP|EXPLAIN|INSERT|REPLACE|SELECT|UPDATE)"
+    )
+    (#set! injection.language "sql")
+)
+
+(encapsed_string
+    (string_content) @injection.content
+    (#match?
+        @injection.content
+        "^\s*(alter|create|delete|drop|explain|insert|replace|select|update)"
+    )
+    (#set! injection.language "sql")
+)
