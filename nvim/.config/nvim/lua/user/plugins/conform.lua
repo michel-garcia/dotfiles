f6151669c713a44eb063ce331b83331f1881f055
Michel Garcia <michelgarxia@gmail.com>
2024-07-08 14:16:10 -0400

    feat(nvim): add formatters for c, cpp, lua, php and python

--- nvim/.config/nvim/lua/user/plugins/conform.lua
+++ nvim/.config/nvim/lua/user/plugins/conform.lua
@@ -4,9 +4,13 @@ return {
         local conform = require("conform")
         conform.setup({
             formatters_by_ft = {
+                c = { "astyle" },
+                cpp = { "astyle" },
                 javascript = { "prettier" },
                 javascriptreact = { "prettier" },
-                php = { "pretty-php" }
+                lua = { "stylua" },
+                php = { "pretty-php" },
+                python = { "black" },
             }
         })
         vim.keymap.set("n", "<A-f>", function()
