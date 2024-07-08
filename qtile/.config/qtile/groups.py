b86ed8edd8b86f0bbea47e97cf75f5b757acf8af
Michel Garcia <michelgarxia@gmail.com>
2024-07-08 15:48:49 -0400

    feat(qtile): add scratchpad terminal

--- qtile/.config/qtile/groups.py
+++ qtile/.config/qtile/groups.py
@@ -1,3 +1,25 @@
-from libqtile.config import Group
+from libqtile.config import DropDown, Group, ScratchPad
+from libqtile.utils import guess_terminal
 
-groups = [Group(i) for i in "123456789"]
+terminal = guess_terminal() or "kitty"
+match(terminal):
+    case "kitty":
+        cmd = "kitty --override background_opacity=.9"
+    case _:
+        cmd = terminal
+
+groups = [
+    *[Group(i) for i in "123456789"],
+    ScratchPad("scratchpad", [
+        DropDown(
+            "term",
+            cmd,
+            height=.95,
+            on_focus_lost_hide=True,
+            opacity=1,
+            width=.95,
+            x=.025,
+            y=.025,
+        )
+    ])
+]
