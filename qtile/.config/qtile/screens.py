a5b2c1f0d266a77914260cb746c4218d7876e695
Michel Garcia <michelgarxia@gmail.com>
2024-07-16 09:06:37 -0400

    fix(qtile): initialize widgets per screen

--- qtile/.config/qtile/screens.py
+++ qtile/.config/qtile/screens.py
@@ -5,14 +5,13 @@ from widgets import init_widgets
 
 
 def init_screens():
-    widgets = init_widgets()
     return [
         Screen(
             bottom=bar.Gap(4),
             left=bar.Gap(4),
             right=bar.Gap(4),
             top=bar.Bar(
-                widgets,
+                init_widgets(),
                 background="#000",
                 margin=[0, 0, 4, 0],
                 size=32,
