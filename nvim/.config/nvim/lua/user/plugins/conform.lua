03414a38f5c00d3ac53998d488fd5079d385cdb2
Michel Garcia <michelgarxia@gmail.com>
2024-09-04 20:40:07 -0400

    feat(nvim): improve formatters configurations

--- nvim/.config/nvim/lua/user/plugins/conform.lua
+++ nvim/.config/nvim/lua/user/plugins/conform.lua
@@ -3,6 +3,22 @@ return {
     config = function()
         local conform = require("conform")
         conform.setup({
+            formatters = {
+                prettier = {
+                    prepend_args = {
+                        "--tab-width",
+                        4,
+                    },
+                },
+                stylua = {
+                    prepend_args = {
+                        "--indent-type",
+                        "Spaces",
+                        "--indent-width",
+                        4,
+                    },
+                },
+            },
             formatters_by_ft = {
                 c = { "astyle" },
                 cpp = { "astyle" },
@@ -12,16 +28,16 @@ return {
                 lua = { "stylua" },
                 php = { "pretty-php" },
                 python = { "black" },
-            }
+            },
         })
         vim.keymap.set("n", "<A-f>", function()
             conform.format({
                 async = true,
-                lsp_fallback = false
+                lsp_fallback = false,
             })
         end, {
             noremap = true,
-            silent = true
+            silent = true,
         })
-    end
+    end,
 }
