3e0957a3262e3be36bbb433c8b7315a6d1fbd85a
Michel Garcia <michelgarxia@gmail.com>
2024-09-24 14:03:00 -0400

    feat(qtile): add key bindings for screen navigation

--- qtile/.config/qtile/input.py
+++ qtile/.config/qtile/input.py
@@ -8,6 +8,23 @@ from libqtile.utils import guess_terminal
 mod = "mod4"
 terminal = guess_terminal()
 
+
+def window_to_next_screen(qtile):
+    current_index = qtile.screens.index(qtile.current_screen)
+    index = (current_index + 1) % len(qtile.screens)
+    screen = qtile.screens[index]
+    qtile.current_window.togroup(screen.group.name)
+    qtile.cmd_to_screen(index)
+
+
+def window_to_prev_screen(qtile):
+    current_index = qtile.screens.index(qtile.current_screen)
+    index = (current_index - 1) % len(qtile.screens)
+    screen = qtile.screens[index]
+    qtile.current_window.togroup(screen.group.name)
+    qtile.cmd_to_screen(index)
+
+
 def init_keys(groups):
     keys = [
         Key([mod], "h", lazy.layout.left()),
@@ -15,11 +32,15 @@ def init_keys(groups):
         Key([mod], "j", lazy.layout.down()),
         Key([mod], "k", lazy.layout.up()),
         Key([mod], "Tab", lazy.group.next_window()),
+        Key([mod], "period", lazy.next_screen()),
+        Key([mod], "comma", lazy.prev_screen()),
         Key([mod, "shift"], "Tab", lazy.group.prev_window()),
         Key([mod, "shift"], "h", lazy.layout.shuffle_left()),
         Key([mod, "shift"], "l", lazy.layout.shuffle_right()),
         Key([mod, "shift"], "j", lazy.layout.shuffle_down()),
         Key([mod, "shift"], "k", lazy.layout.shuffle_up()),
+        Key([mod, "shift"], "period", lazy.function(window_to_next_screen)),
+        Key([mod, "shift"], "comma", lazy.function(window_to_prev_screen)),
         Key([mod, "control"], "h", lazy.layout.grow_left()),
         Key([mod, "control"], "l", lazy.layout.grow_right()),
         Key([mod, "control"], "j", lazy.layout.grow_down()),
@@ -34,9 +55,19 @@ def init_keys(groups):
         Key([mod], "r", lazy.spawn("wofi --show drun")),
         Key([], "XF86MonBrightnessUp", lazy.spawn("xbacklight -inc 10")),
         Key([], "XF86MonBrightnessDown", lazy.spawn("xbacklight -dec 10")),
-        Key([], "XF86AudioMute", lazy.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle")),
-        Key([], "XF86AudioLowerVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ -5%")),
-        Key([], "XF86AudioRaiseVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ +5%")),
+        Key(
+            [], "XF86AudioMute", lazy.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle")
+        ),
+        Key(
+            [],
+            "XF86AudioLowerVolume",
+            lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ -5%"),
+        ),
+        Key(
+            [],
+            "XF86AudioRaiseVolume",
+            lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ +5%"),
+        ),
     ]
     for vt in range(1, 8):
         keys.append(
@@ -47,27 +78,36 @@ def init_keys(groups):
             )
         )
     for group in groups:
-        keys.extend([
-            Key([mod], group.name, lazy.group[group.name].toscreen()),
-            Key(
-                [mod, "shift"],
-                group.name,
-                lazy.window.togroup(group.name, switch_group=True),
-            ),
-        ])
+        keys.extend(
+            [
+                Key([mod], group.name, lazy.group[group.name].toscreen()),
+                Key(
+                    [mod, "shift"],
+                    group.name,
+                    lazy.window.togroup(group.name, switch_group=True),
+                ),
+            ]
+        )
     return keys
+
 
 def init_mouse():
     return [
-        Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
-        Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+        Drag(
+            [mod],
+            "Button1",
+            lazy.window.set_position_floating(),
+            start=lazy.window.get_position(),
+        ),
+        Drag(
+            [mod],
+            "Button3",
+            lazy.window.set_size_floating(),
+            start=lazy.window.get_size(),
+        ),
         Click([mod], "Button2", lazy.window.bring_to_front()),
     ]
 
+
 def init_input_rules():
-    return {
-        "type:keyboard": InputConfig(
-            kb_layout="us",
-            kb_variant="altgr-intl"
-        )
-    }
+    return {"type:keyboard": InputConfig(kb_layout="us", kb_variant="altgr-intl")}
