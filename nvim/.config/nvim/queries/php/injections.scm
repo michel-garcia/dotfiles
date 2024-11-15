02f905880fdb15d8331bee862cbc8781b0f63e1b
Michel Garcia <michelgarxia@gmail.com>
2024-11-15 16:03:53 -0500

    feat(nvim): improve sql language injections in php files

--- nvim/.config/nvim/queries/php/injections.scm
+++ nvim/.config/nvim/queries/php/injections.scm
@@ -1,19 +1,121 @@
 ; extends
 
-(encapsed_string
+(string
     (string_content) @injection.content
-    (#match?
-        @injection.content
-        "^\s*(ALTER|CREATE|DELETE|DROP|EXPLAIN|INSERT|REPLACE|SELECT|UPDATE)"
-    )
+    (#any-lua-match? @injection.content "^%s*[Aa][Ll][Tt][Ee][Rr]")
     (#set! injection.language "sql")
 )
 
 (encapsed_string
     (string_content) @injection.content
-    (#match?
-        @injection.content
-        "^\s*(alter|create|delete|drop|explain|insert|replace|select|update)"
-    )
+    (#any-lua-match? @injection.content "^%s*[Aa][Ll][Tt][Ee][Rr]")
+    (#set! injection.language "sql")
+)
+
+(string
+    (string_content) @injection.content
+    (#any-lua-match? @injection.content "^%s*[Cc][Rr][Ee][Aa][Tt][Ee]")
+    (#set! injection.language "sql")
+)
+
+(encapsed_string
+    (string_content) @injection.content
+    (#any-lua-match? @injection.content "^%s*[Cc][Rr][Ee][Aa][Tt][Ee]")
+    (#set! injection.language "sql")
+)
+
+(string
+    (string_content) @injection.content
+    (#any-lua-match? @injection.content "^%s*[Dd][Ee][Ll][Ee][Tt][Ee]")
+    (#set! injection.language "sql")
+)
+
+(encapsed_string
+    (string_content) @injection.content
+    (#any-lua-match? @injection.content "^%s*[Dd][Ee][Ll][Ee][Tt][Ee]")
+    (#set! injection.language "sql")
+)
+
+(string
+    (string_content) @injection.content
+    (#any-lua-match? @injection.content "^%s*[Dd][Rr][Oo][Pp]")
+    (#set! injection.language "sql")
+)
+
+(encapsed_string
+    (string_content) @injection.content
+    (#any-lua-match? @injection.content "^%s*[Dd][Rr][Oo][Pp]")
+    (#set! injection.language "sql")
+)
+
+(string
+    (string_content) @injection.content
+    (#any-lua-match? @injection.content "^%s*[Ee][Xx][Pp][Ll][Aa][Ii][Nn]")
+    (#set! injection.language "sql")
+)
+
+(encapsed_string
+    (string_content) @injection.content
+    (#any-lua-match? @injection.content "^%s*[Ee][Xx][Pp][Ll][Aa][Ii][Nn]")
+    (#set! injection.language "sql")
+)
+
+(string
+    (string_content) @injection.content
+    (#any-lua-match? @injection.content "^%s*[Ii][Nn][Ss][Ee][Rr][Tt]")
+    (#set! injection.language "sql")
+)
+
+(encapsed_string
+    (string_content) @injection.content
+    (#any-lua-match? @injection.content "^%s*[Ii][Nn][Ss][Ee][Rr][Tt]")
+    (#set! injection.language "sql")
+)
+
+(string
+    (string_content) @injection.content
+    (#any-lua-match? @injection.content "^%s*[Rr][Ee][Pp][Ll][Aa][Cc][Ee]")
+    (#set! injection.language "sql")
+)
+
+(encapsed_string
+    (string_content) @injection.content
+    (#any-lua-match? @injection.content "^%s*[Rr][Ee][Pp][Ll][Aa][Cc][Ee]")
+    (#set! injection.language "sql")
+)
+
+(string
+    (string_content) @injection.content
+    (#any-lua-match? @injection.content "^%s*[Ss][Ee][Ll][Ee][Cc][Tt]")
+    (#set! injection.language "sql")
+)
+
+(encapsed_string
+    (string_content) @injection.content
+    (#any-lua-match? @injection.content "^%s*[Ss][Ee][Ll][Ee][Cc][Tt]")
+    (#set! injection.language "sql")
+)
+
+(string
+    (string_content) @injection.content
+    (#any-lua-match? @injection.content "^%s*[Ss][Hh][Oo][Ww]")
+    (#set! injection.language "sql")
+)
+
+(encapsed_string
+    (string_content) @injection.content
+    (#any-lua-match? @injection.content "^%s*[Ss][Hh][Oo][Ww]")
+    (#set! injection.language "sql")
+)
+
+(string
+    (string_content) @injection.content
+    (#any-lua-match? @injection.content "^%s*[Uu][Pp][Dd][Aa][Tt][Ee]")
+    (#set! injection.language "sql")
+)
+
+(encapsed_string
+    (string_content) @injection.content
+    (#any-lua-match? @injection.content "^%s*[Uu][Pp][Dd][Aa][Tt][Ee]")
     (#set! injection.language "sql")
 )
