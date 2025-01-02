c458af385b5470587e957d44e17acb841bb26e10
Michel Garcia <michelgarxia@gmail.com>
2025-01-02 15:48:20 -0500

    refactor(nvim): improve key mapping setup

--- nvim/.config/nvim/lua/user/core/keymaps.lua
+++ nvim/.config/nvim/lua/user/core/keymaps.lua
@@ -1,72 +1,62 @@
-local mappings = {
-    general = {
-        bind = function (opts)
-            vim.keymap.set({ "n", "x" }, "j", function ()
-                vim.api.nvim_feedkeys(table.concat({
-                    vim.v.count > 0 and vim.v.count or "",
-                    vim.v.count > 0 and "j" or "gj"
-                }), "n", false)
-            end, opts)
-            vim.keymap.set({ "n", "x" }, "k", function ()
-                vim.api.nvim_feedkeys(table.concat({
-                    vim.v.count > 0 and vim.v.count or "",
-                    vim.v.count > 0 and "k" or "gk"
-                }), "n", false)
-            end, opts)
-            vim.keymap.set("n", "<C-s>", "<cmd>wincmd s<CR>", opts)
-            vim.keymap.set("n", "<C-v>", "<cmd>wincmd v<CR>", opts)
-            vim.keymap.set("n", "<C-h>", "<cmd>wincmd h<CR>", opts)
-            vim.keymap.set("n", "<C-j>", "<cmd>wincmd j<CR>", opts)
-            vim.keymap.set("n", "<C-k>", "<cmd>wincmd k<CR>", opts)
-            vim.keymap.set("n", "<C-l>", "<cmd>wincmd l<CR>", opts)
-            vim.keymap.set("n", "<C-t>", "<cmd>$tabnew<CR>", opts)
-            vim.keymap.set("n", "<C-w>", "<cmd>tabclose<CR>", vim.tbl_extend('force', opts, {
-                nowait = true
-            }))
-            vim.keymap.set("n", "<C-,>", "<cmd>tabprevious<CR>", opts)
-            vim.keymap.set("n", "<C-.>", "<cmd>tabnext<CR>", opts)
-            vim.keymap.set("v", "<", "<gv", opts)
-            vim.keymap.set("v", ">", ">gv", opts)
-            vim.keymap.set("v", "<A-j>", ":move '>+1<CR>gv=gv", opts)
-            vim.keymap.set("v", "<A-k>", ":move '<-2<CR>gv=gv", opts)
-            vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", opts)
-            vim.keymap.set("t", "<A-h>", "<C-\\><C-n><cmd>wincmd h<CR>", opts)
-            vim.keymap.set("t", "<A-j>", "<C-\\><C-n><cmd>wincmd j<CR>", opts)
-            vim.keymap.set("t", "<A-k>", "<C-\\><C-n><cmd>wincmd k<CR>", opts)
-            vim.keymap.set("t", "<A-l>", "<C-\\><C-n><cmd>wincmd l<CR>", opts)
-        end
-    },
-    lsp = {
-        bind = function (opts)
-            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-            vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
-            vim.keymap.set("n", "<F4>", vim.lsp.buf.code_action, opts)
-            vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-            vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
-            vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-            vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-        end
-    }
-}
+local map = function(mode, lhs, rhs, opts)
+    opts = vim.tbl_extend("force", {
+        noremap = true,
+        silent = true,
+    }, opts or {})
+    vim.keymap.set(mode, lhs, rhs, opts)
+end
 
-mappings.general.bind({
-    noremap = true,
-    silent = true
-})
+map({ "n", "x" }, "j", function()
+    local keys = table.concat({
+        vim.v.count > 0 and vim.v.count or "",
+        vim.v.count > 0 and "j" or "gj",
+    })
+    vim.api.nvim_feedkeys(keys, "n", false)
+end)
+map({ "n", "x" }, "k", function()
+    local keys = table.concat({
+        vim.v.count > 0 and vim.v.count or "",
+        vim.v.count > 0 and "k" or "gk",
+    })
+    vim.api.nvim_feedkeys(keys, "n", false)
+end)
+map("n", "<C-s>", "<cmd>wincmd s<CR>")
+map("n", "<C-v>", "<cmd>wincmd v<CR>")
+map("n", "<C-h>", "<cmd>wincmd h<CR>")
+map("n", "<C-j>", "<cmd>wincmd j<CR>")
+map("n", "<C-k>", "<cmd>wincmd k<CR>")
+map("n", "<C-l>", "<cmd>wincmd l<CR>")
+map("n", "<C-t>", "<cmd>$tabnew<CR>")
+map("n", "<C-w>", "<cmd>tabclose<CR>", { nowait = true })
+map("n", "<C-,>", "<cmd>tabprevious<CR>")
+map("n", "<C-.>", "<cmd>tabnext<CR>")
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+map("v", "<A-j>", ":move '>+1<CR>gv=gv")
+map("v", "<A-k>", ":move '<-2<CR>gv=gv")
+map("t", "<Esc>", "<C-\\><C-n>")
+map("t", "<A-h>", "<C-\\><C-n><cmd>wincmd h<CR>")
+map("t", "<A-j>", "<C-\\><C-n><cmd>wincmd j<CR>")
+map("t", "<A-k>", "<C-\\><C-n><cmd>wincmd k<CR>")
+map("t", "<A-l>", "<C-\\><C-n><cmd>wincmd l<CR>")
 
 vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function (args)
-        mappings.lsp.bind({
+    callback = function(args)
+        local opts = {
             buffer = args.buf,
-            noremap = true,
-            silent = true
-        })
+        }
+        map("n", "gD", vim.lsp.buf.declaration, opts)
+        map("n", "gd", vim.lsp.buf.definition, opts)
+        map("n", "K", vim.lsp.buf.hover, opts)
+        map("n", "gi", vim.lsp.buf.implementation, opts)
+        map("n", "<F2>", vim.lsp.buf.rename, opts)
+        map("n", "<F4>", vim.lsp.buf.code_action, opts)
+        map("n", "gr", vim.lsp.buf.references, opts)
+        map("n", "gl", vim.diagnostic.open_float, opts)
+        map("n", "[d", vim.diagnostic.goto_next, opts)
+        map("n", "]d", vim.diagnostic.goto_prev, opts)
     end,
     group = vim.api.nvim_create_augroup("LspMappings", {
-        clear = true
-    })
+        clear = true,
+    }),
 })
-
