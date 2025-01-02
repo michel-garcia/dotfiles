93b785d93b0f669aa5e7aee1cc918792a0a44d83
Michel Garcia <michelgarxia@gmail.com>
2025-01-02 10:08:47 -0500

    feat(nvim): add edgy plugin

---
+++ nvim/.config/nvim/lua/user/plugins/edgy.lua
@@ -0,0 +1,17 @@
+return {
+    "folke/edgy.nvim",
+    event = "VeryLazy",
+    opts = {
+        bottom = {
+            {
+                filter = function(_, win)
+                    return vim.api.nvim_win_get_config(win).relative == ""
+                end,
+                ft = "toggleterm",
+                size = {
+                    height = 0.25,
+                },
+            },
+        },
+    },
+}
