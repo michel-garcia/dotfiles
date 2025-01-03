c4eb906dfb3dde24df84b611c7428aceef4cfcab
Michel Garcia <michelgarxia@gmail.com>
2025-01-03 13:28:30 -0500

    feat(nvim): improve toggleterm key mappings

--- nvim/.config/nvim/lua/user/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/user/plugins/toggleterm.lua
@@ -1,23 +1,27 @@
 return {
     "akinsho/toggleterm.nvim",
-    config = function ()
+    config = function()
         local toggleterm = require("toggleterm")
         toggleterm.setup({
             persist_mode = false,
             shade_terminals = false,
-            size = 15
+            size = 15,
         })
-        vim.keymap.set({ "n", "t" }, "<C-\\>", "<Cmd>ToggleTerm<CR>", {
-            noremap = true,
-            silent = true
-        })
-        for i = 1, 9 do
-            vim.keymap.set({ "n", "t" }, string.format("<C-%s>", i), function ()
-                toggleterm.toggle(i)
-            end, {
+        for n = 1, 9 do
+            local kopts = {
                 noremap = true,
-                silent = true
-            })
+                silent = true,
+            }
+            vim.keymap.set({ "n", "t" }, string.format("<A-%s>", n), function()
+                local instances = require("toggleterm.terminal")
+                local terminals = instances.get_all()
+                for _, terminal in ipairs(terminals) do
+                    if terminal.id ~= n then
+                        terminal:close()
+                    end
+                end
+                toggleterm.toggle(n)
+            end, kopts)
         end
-    end
+    end,
 }
