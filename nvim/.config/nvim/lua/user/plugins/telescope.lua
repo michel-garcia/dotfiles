3c477c081d13f44a04b711e83cc0cb620221f810
Michel Garcia <michelgarxia@gmail.com>
2025-01-24 16:14:24 -0500

    refactor(nvim): improve telescope config

--- nvim/.config/nvim/lua/user/plugins/telescope.lua
+++ nvim/.config/nvim/lua/user/plugins/telescope.lua
@@ -1,105 +1,111 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    version = false,
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        {
-            "nvim-telescope/telescope-fzf-native.nvim",
-            build = "make",
-            enabled = vim.fn.executable("make") == 1
+    {
+        "nvim-telescope/telescope.nvim",
+        version = false,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
         },
-        {
-            "nvim-telescope/telescope-file-browser.nvim",
-            dependencies = {
-                "nvim-tree/nvim-web-devicons"
+        config = function(_, opts)
+            local telescope = require("telescope")
+            telescope.setup(opts)
+            local builtin = require("telescope.builtin")
+            local kopts = {
+                noremap = true,
+                silent = true,
+            }
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, kopts)
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, kopts)
+            vim.keymap.set("n", "<leader>fb", builtin.buffers, kopts)
+            vim.keymap.set("n", "<leader>fh", builtin.help_tags, kopts)
+            vim.api.nvim_set_hl(0, "TelescopeNormal", {
+                link = "NormalFloat",
+            })
+            vim.api.nvim_set_hl(0, "TelescopeBorder", {
+                link = "FloatBorder",
+            })
+            vim.api.nvim_set_hl(0, "TelescopePromptTitle", {
+                link = "lualine_a_insert",
+            })
+        end,
+        opts = {
+            defaults = {
+                borderchars = { " ", " ", " ", " ", " ", " ", " ", " " },
+                color_devicons = false,
+                layout_config = {
+                    height = function(_, _, lines)
+                        return math.max(math.ceil(lines * 0.4), 15)
+                    end,
+                    width = function(_, columns)
+                        return math.max(math.ceil(columns * 0.5), 70)
+                    end,
+                },
+                layout_strategy = "center",
+                preview = false,
+                results_title = false,
+                sorting_strategy = "ascending",
             },
-            config = function ()
-                local telescope = require("telescope")
-                vim.keymap.set("n", "<leader>fe", function ()
-                    telescope.extensions.file_browser.file_browser({
-                        cwd = vim.fn.resolve(vim.fn.expand("%:p:h"))
-                    })
-                end, {
-                    noremap = true,
-                    silent = true
+            extensions = {
+                file_browser = {
+                    create_from_prompt = false,
+                    display_stat = false,
+                    git_status = false,
+                    grouped = true,
+                    hide_parent_dir = true,
+                    hidden = true,
+                },
+            },
+            pickers = {
+                find_files = {
+                    hidden = true,
+                },
+            },
+        },
+    },
+    {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        config = function()
+            local telescope = require("telescope")
+            telescope.load_extension("fzf")
+        end,
+        enabled = vim.fn.executable("make") == 1,
+    },
+    {
+        "nvim-telescope/telescope-file-browser.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            local telescope = require("telescope")
+            telescope.load_extension("file_browser")
+            local kopts = {
+                noremap = true,
+                silent = true,
+            }
+            vim.keymap.set("n", "<leader>fe", function()
+                telescope.extensions.file_browser.file_browser({
+                    cwd = vim.fn.resolve(vim.fn.expand("%:p:h")),
                 })
-                local group = vim.api.nvim_create_augroup("TelescopeFileBrowser", {
-                    clear = true
-                })
-                vim.api.nvim_create_autocmd("VimEnter", {
-                    callback = function (args)
-                        if vim.fn.isdirectory(args.file) ~= 0 then
-                            local name = vim.api.nvim_buf_get_name(0)
-                            if name == args.file then
-                                vim.api.nvim_buf_delete(0, {
-                                    force = true
-                                })
-                            end
-                            telescope.extensions.file_browser.file_browser({
-                                cwd = vim.fn.resolve(args.file)
+            end, kopts)
+            local group = vim.api.nvim_create_augroup("TelescopeFileBrowser", {
+                clear = true,
+            })
+            vim.api.nvim_create_autocmd("VimEnter", {
+                callback = function(args)
+                    if vim.fn.isdirectory(args.file) ~= 0 then
+                        local name = vim.api.nvim_buf_get_name(0)
+                        if name == args.file then
+                            vim.api.nvim_buf_delete(0, {
+                                force = true,
                             })
                         end
-                    end,
-                    group = group
-                })
-            end
-        }
-    },
-    opts = {
-        defaults = {
-            borderchars = { " ", " ", " ", " ", " ", " ", " ", " " },
-            color_devicons = false,
-            layout_config = {
-                height = function (_, _, lines)
-                    return math.max(math.ceil(lines * .4), 15)
+                        telescope.extensions.file_browser.file_browser({
+                            cwd = vim.fn.resolve(args.file),
+                        })
+                    end
                 end,
-                width = function (_, columns)
-                    return math.max(math.ceil(columns * .5), 70)
-                end
-            },
-            layout_strategy = "center",
-            preview = false,
-            results_title = false,
-            sorting_strategy = "ascending"
-        },
-        extensions = {
-            file_browser = {
-                create_from_prompt = false,
-                display_stat = false,
-                git_status = false,
-                grouped = true,
-                hide_parent_dir = true,
-                hidden = true
-            }
-        },
-        pickers = {
-            find_files = {
-                hidden = true
-            }
-        }
+                group = group,
+            })
+        end,
     },
-    config = function(_, opts)
-        local telescope = require("telescope")
-        telescope.setup(opts)
-        telescope.load_extension("fzf")
-        telescope.load_extension("file_browser")
-        local builtin = require("telescope.builtin")
-        local k_opts = {
-            noremap = true,
-            silent = true
-        }
-        vim.keymap.set("n", "<leader>ff", builtin.find_files, k_opts)
-        vim.keymap.set("n", "<leader>fg", builtin.live_grep, k_opts)
-        vim.keymap.set("n", "<leader>fb", builtin.buffers, k_opts)
-        vim.keymap.set("n", "<leader>fh", builtin.help_tags, k_opts)
-        vim.api.nvim_set_hl(0, "TelescopeNormal", {
-            link = "NormalFloat"
-        })
-        vim.api.nvim_set_hl(0, "TelescopeBorder", {
-            link = "FloatBorder"
-        })
-        vim.api.nvim_set_hl(0, "TelescopePromptTitle", {
-            link = "lualine_a_insert"
-        })
-    end
 }
