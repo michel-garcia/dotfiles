9319b9a19a2692dfb0f560b26d02f8701ad20cb4
Michel Garcia <michelgarxia@gmail.com>
2025-01-29 22:46:04 -0500

    feat(nvim): add support for wsl clipboard

--- nvim/.config/nvim/lua/user/core/vars.lua
+++ nvim/.config/nvim/lua/user/core/vars.lua
@@ -2,3 +2,17 @@ vim.g.mapleader = " "
 vim.g.netrw_banner = 0
 vim.g.netrw_fastbrowse = 0
 
+if vim.fn.has("wsl") then
+    vim.g.clipboard = {
+        name = "WslClipboard",
+        copy = {
+            ["+"] = "clip.exe",
+            ["*"] = "clip.exe",
+        },
+        paste = {
+            ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        },
+        cache_enabled = false,
+    }
+end
