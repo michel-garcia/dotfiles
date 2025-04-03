6afba30515ad88207d7e696de1a2dd150c12f033
Michel Garcia <michelgarxia@gmail.com>
2025-04-03 09:05:58 -0400

    refactor(nvim): improve colorscheme config

--- nvim/.config/nvim/lua/user/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/user/plugins/colorscheme.lua
@@ -1,29 +1,29 @@
 return {
     "luisiacc/gruvbox-baby",
     priority = 1000,
-    config = function ()
-        vim.g.gruvbox_baby_background_color = "dark"
+    config = function()
+        vim.g.gruvbox_baby_highlights = {
+            DiagnosticUnderlineError = {
+                sp = "red",
+                style = "undercurl",
+            },
+            DiagnosticUnderlineWarn = {
+                sp = "orange",
+                style = "undercurl",
+            },
+            DiagnosticUnderlineInfo = {
+                sp = "lightblue",
+                style = "undercurl",
+            },
+            DiagnosticUnderlineHint = {
+                sp = "lightgray",
+                style = "undercurl",
+            },
+            WinSeparator = {
+                link = "@comment",
+            },
+        }
         vim.g.gruvbox_baby_transparent_mode = true
         vim.cmd.colorscheme("gruvbox-baby")
-        vim.api.nvim_set_hl(0, "WinSeparator", {
-            link = "IndentBlanklineChar"
-        })
-        vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", {
-            sp = "red",
-            undercurl = true
-        })
-        vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", {
-            sp = "orange",
-            undercurl = true
-        })
-        vim.api.nvim_set_hl(0, "DiagnosticUnderlineInfo", {
-            sp = "lightblue",
-            undercurl = true
-        })
-        vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", {
-            sp = "lightgray",
-            undercurl = true
-        })
-    end
+    end,
 }
-
