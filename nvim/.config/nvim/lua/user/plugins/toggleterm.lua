69899fc64dfcb6d3b3522ed7646ab9144e7b87fe
Michel Garcia <michelgarxia@gmail.com>
2025-04-29 10:18:59 -0400

    refactor(nvim): improve terminal plugin config

--- nvim/.config/nvim/lua/user/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/user/plugins/toggleterm.lua
@@ -1,27 +1,32 @@
 return {
     "akinsho/toggleterm.nvim",
-    config = function()
+    opts = {
+        persist_mode = false,
+        shade_terminals = false,
+    },
+    config = function(_, opts)
         local toggleterm = require("toggleterm")
-        toggleterm.setup({
-            persist_mode = false,
-            shade_terminals = false,
-            size = 15,
-        })
-        for n = 1, 9 do
-            local kopts = {
-                noremap = true,
-                silent = true,
-            }
-            vim.keymap.set({ "n", "t" }, string.format("<A-%s>", n), function()
-                local instances = require("toggleterm.terminal")
+        toggleterm.setup(opts)
+        local instances = require("toggleterm.terminal")
+        for id = 1, 9 do
+            vim.keymap.set({ "n", "t" }, string.format("<C-%s>", id), function()
                 local terminals = instances.get_all()
                 for _, terminal in ipairs(terminals) do
-                    if terminal.id ~= n then
+                    if terminal.id ~= id then
                         terminal:close()
                     end
                 end
-                toggleterm.toggle(n)
-            end, kopts)
+                toggleterm.toggle(id)
+                vim.schedule(function()
+                    local type = vim.api.nvim_get_option_value("buftype", {})
+                    if type == "terminal" then
+                        vim.cmd("startinsert")
+                    end
+                end)
+            end, {
+                noremap = true,
+                silent = true,
+            })
         end
     end,
 }
