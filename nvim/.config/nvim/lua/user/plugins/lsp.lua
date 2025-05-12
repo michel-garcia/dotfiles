23606c33c6962d22cc6331c902b71cfd2573c853
Michel Garcia <michelgarxia@gmail.com>
2025-05-12 10:04:57 -0400

    feat(nvim): update lsp config

--- nvim/.config/nvim/lua/user/plugins/lsp.lua
+++ nvim/.config/nvim/lua/user/plugins/lsp.lua
@@ -1,45 +1,16 @@
 return {
     {
-        "williamboman/mason.nvim",
-        dependencies = {
-            "williamboman/mason-lspconfig.nvim",
-            "neovim/nvim-lspconfig",
+        "mason-org/mason.nvim",
+        opts = {
+            ui = {
+                border = "solid",
+                icons = {
+                    package_installed = "󰄬",
+                    package_pending = "󰁅",
+                    package_uninstalled = "󰅖",
+                },
+            },
         },
-        config = function()
-            local mason = require("mason")
-            mason.setup({
-                ui = {
-                    border = "solid",
-                    icons = {
-                        package_installed = "󰄬",
-                        package_pending = "󰁅",
-                        package_uninstalled = "󰅖",
-                    },
-                },
-            })
-            local lsp = require("mason-lspconfig")
-            lsp.setup({
-                ensure_installed = {
-                    "clangd",
-                    "intelephense",
-                    "jsonls",
-                    "lua_ls",
-                    "ts_ls",
-                },
-            })
-            local lspconfig = require("lspconfig")
-            lsp.setup_handlers({
-                function(server_name)
-                    local cmp = require("cmp_nvim_lsp")
-                    local path = string.format("user.lsp.%s", server_name)
-                    local ok, config = pcall(require, path)
-                    local opts = vim.tbl_deep_extend("force", {
-                        capabilities = cmp.default_capabilities(),
-                    }, ok and config or {})
-                    lspconfig[server_name].setup(opts)
-                end,
-            })
-        end,
     },
     {
         "neovim/nvim-lspconfig",
@@ -47,5 +18,21 @@ return {
             local win = require("lspconfig.ui.windows")
             win.default_options.border = "solid"
         end,
+    },
+    {
+        "mason-org/mason-lspconfig.nvim",
+        dependencies = {
+            "mason-org/mason.nvim",
+            "neovim/nvim-lspconfig",
+        },
+        opts = {
+            ensure_installed = {
+                "clangd",
+                "intelephense",
+                "jsonls",
+                "lua_ls",
+                "ts_ls",
+            },
+        },
     },
 }
