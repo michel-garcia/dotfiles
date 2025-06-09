2a64ccc5833278aabd2ed1051f6e30ca6e0c87fd
Michel Garcia <michelgarxia@gmail.com>
2025-06-09 14:36:43 -0400

    refactor(nvim): merge mini plugins configs

--- nvim/.config/nvim/lua/user/plugins/cursorword.lua
+++
@@ -1,6 +0,0 @@
-return {
-    "echasnovski/mini.cursorword",
-    opts = {
-        delay = 0,
-    },
-}
---
+++ nvim/.config/nvim/lua/user/plugins/mini.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        "echasnovski/mini.cursorword",
+        opts = {
+            delay = 0,
+        },
+    },
+    {
+        "echasnovski/mini.surround",
+        config = true,
+    },
+    {
+        "echasnovski/mini.splitjoin",
+        config = true,
+    },
+}
