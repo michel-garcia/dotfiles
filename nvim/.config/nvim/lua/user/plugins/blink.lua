bc428cb1ac30f03ed4ae21c6fca3450ddb52884d
Michel Garcia <michelgarxia@gmail.com>
2025-07-08 10:08:56 -0400

    feat(nvim): replace neodev with lazydev plugin

--- nvim/.config/nvim/lua/user/plugins/blink.lua
+++ nvim/.config/nvim/lua/user/plugins/blink.lua
@@ -44,5 +44,17 @@ return {
                 border = "solid",
             },
         },
+        sources = {
+            default = {
+                "lazydev", "lsp", "path", "snippets", "buffer",
+            },
+            providers = {
+                lazydev = {
+                    name = "LazyDev",
+                    module = "lazydev.integrations.blink",
+                    score_offset = 100,
+                },
+            },
+        },
     },
 }
