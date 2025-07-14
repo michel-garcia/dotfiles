37a29670de1409313355e3613f2dc94d628dd403
Michel Garcia <michelgarxia@gmail.com>
2025-07-14 12:57:42 -0400

    feat(nvim): replace notify plugin with snacks notifier

--- nvim/.config/nvim/lua/user/plugins/snacks.lua
+++ nvim/.config/nvim/lua/user/plugins/snacks.lua
@@ -38,6 +38,31 @@ return {
                 enabled = false,
             },
         },
+        notifier = {
+            margin = {
+                bottom = 1,
+            },
+            style = function(buf, notification, context)
+                context.opts.border = "solid"
+                context.opts.wo.winhighlight = table.concat({
+                    "Normal:NormalFloat",
+                    "NormalNC:NormalFloat",
+                    "FloatBorder:NormalFloat",
+                }, ",")
+                local message = vim.split(notification.msg, "\n")
+                vim.api.nvim_buf_set_lines(buf, 0, -1, false, message)
+                vim.api.nvim_buf_set_extmark(buf, context.ns, 0, 0, {
+                    virt_text = {
+                        {
+                            notification.icon,
+                            context.hl.icon,
+                        },
+                    },
+                    virt_text_pos = "right_align",
+                })
+            end,
+            top_down = false,
+        },
         styles = {
             dashboard = {
                 wo = {
