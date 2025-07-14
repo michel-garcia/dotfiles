b493b7976913bc0f7a89d1da700fa1275e1f7d68
Michel Garcia <michelgarxia@gmail.com>
2025-07-14 12:58:14 -0400

    feat(nvim): update rsync config

--- nvim/.config/nvim/lua/user/plugins/rsync.lua
+++ nvim/.config/nvim/lua/user/plugins/rsync.lua
@@ -1,10 +1,23 @@
 return {
     "michel-garcia/rsync.nvim",
     dev = true,
-    config = function()
-        local rsync = require("rsync")
-        rsync.setup({
-            sync_up_on_write = true,
-        })
-    end,
+    opts = {
+        on_update = function(event)
+            local frames = { "⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷" }
+            local message = event.job:status()
+            vim.notify(message, event.level, {
+                id = "rsync",
+                opts = function(notification)
+                    if not event.job.completed then
+                        local delta = vim.uv.hrtime() / (1e6 * 80)
+                        local index = math.floor(delta % #frames + 1)
+                        local icon = frames[index]
+                        notification.icon = icon
+                    end
+                end,
+                title = "Rsync",
+            })
+        end,
+        sync_up_on_write = true,
+    },
 }
