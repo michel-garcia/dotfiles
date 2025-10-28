876abd9e9862c85581cd8498fdb3d997e22df9bd
Michel Garcia <michelgarxia@gmail.com>
2025-10-28 15:52:36 -0400

    feat(nvim): disable confirm option

--- nvim/.config/nvim/lua/user/core/opts.lua
+++ nvim/.config/nvim/lua/user/core/opts.lua
@@ -4,7 +4,6 @@ vim.opt.breakindent = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.cmdheight = 0
 vim.opt.colorcolumn = { 80 }
-vim.opt.confirm = true
 vim.opt.encoding = "utf-8"
 vim.opt.expandtab = true
 vim.opt.fillchars = {
