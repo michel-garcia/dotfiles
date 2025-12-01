b45ca689b1aee06b2c19afb373f59ef22b83909d
Michel Garcia <michelgarxia@gmail.com>
2025-12-01 10:25:40 -0500

    feat(nvim): enable line wrap for fzf picker previews

--- nvim/.config/nvim/lua/user/plugins/fzf.lua
+++ nvim/.config/nvim/lua/user/plugins/fzf.lua
@@ -24,6 +24,7 @@ return {
                 border = "single",
                 preview = {
                     border = "single",
+                    wrap = true,
                 },
                 title_flags = false,
             },
