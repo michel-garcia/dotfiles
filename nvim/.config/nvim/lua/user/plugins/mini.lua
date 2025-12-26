9312199815c5fc66d788645870f56d50f05536f8
Michel Garcia <michelgarxia@gmail.com>
2025-12-26 01:35:39 -0500

    feat(nvim): add mini completion plugin

--- nvim/.config/nvim/lua/user/plugins/mini.lua
+++ nvim/.config/nvim/lua/user/plugins/mini.lua
@@ -1,5 +1,23 @@
 return {
     {
+        "nvim-mini/mini.completion",
+        opts = {
+            delay = {
+                completion = 0,
+                info = 0,
+                signature = 0,
+            },
+            window = {
+                info = {
+                    border = "solid",
+                },
+                signature = {
+                    border = "solid",
+                },
+            },
+        },
+    },
+    {
         "nvim-mini/mini.ai",
         config = true,
     },
@@ -8,6 +26,14 @@ return {
         opts = {
             delay = 0,
         },
+    },
+    {
+        "nvim-mini/mini.icons",
+        config = function()
+            local icons = require("mini.icons")
+            icons.setup()
+            icons.tweak_lsp_kind()
+        end,
     },
     {
         "nvim-mini/mini.jump",
