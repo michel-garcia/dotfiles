ef96bf912516e00995bb1d62d40f5638cdf4ebd8
Michel Garcia <michelgarxia@gmail.com>
2026-01-09 08:49:55 -0500

    feat(nvim): remove snacks dashboard

--- nvim/.config/nvim/lua/user/plugins/snacks.lua
+++ nvim/.config/nvim/lua/user/plugins/snacks.lua
@@ -1,34 +1,8 @@
-local version = vim.version()
-
 return {
     "folke/snacks.nvim",
     opts = {
         bigfile = {
             line_length = 1000,
-        },
-        dashboard = {
-            formats = {
-                icon = function()
-                    return {}
-                end,
-            },
-            sections = {
-                {
-                    section = "header",
-                },
-                {
-                    text = {
-                        string.format("Version %s.%s.%s", version.major, version.minor, version.patch),
-                        hl = "@character",
-                    },
-                    align = "center",
-                    padding = 3,
-                },
-                {
-                    section = "keys",
-                    gap = 1,
-                },
-            },
         },
         notifier = {
             margin = {
@@ -54,16 +28,6 @@ return {
                 })
             end,
             top_down = false,
-        },
-        styles = {
-            dashboard = {
-                wo = {
-                    winhighlight = table.concat({
-                        "SnacksDashboardHeader:@function",
-                        "SnacksDashboardDesc:@operator",
-                    }, ","),
-                },
-            },
         },
     },
 }
