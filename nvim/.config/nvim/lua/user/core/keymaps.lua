cd758a34f32af87cb9d22abea1dd0d5a719dc93e
Michel Garcia <michelgarxia@gmail.com>
2026-01-10 04:16:05 -0500

    feat(nvim): refactor lsp keymaps

--- nvim/.config/nvim/lua/user/core/keymaps.lua
+++ nvim/.config/nvim/lua/user/core/keymaps.lua
@@ -24,3 +24,25 @@ vim.keymap.set("v", ">", ">gv")
 vim.keymap.set("v", "<A-j>", ":move '>+1<CR>gv=gv")
 vim.keymap.set("v", "<A-k>", ":move '<-2<CR>gv=gv")
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "K", function()
+    vim.lsp.buf.hover({
+        border = "solid",
+    })
+end)
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
+vim.keymap.set("n", "<F2>", vim.lsp.buf.rename)
+vim.keymap.set("n", "<F4>", vim.lsp.buf.code_action)
+vim.keymap.set("n", "gr", vim.lsp.buf.references)
+vim.keymap.set("n", "gl", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", function()
+    vim.diagnostic.jump({
+        count = 1,
+    })
+end)
+vim.keymap.set("n", "]d", function()
+    vim.diagnostic.jump({
+        count = -1,
+    })
+end)
