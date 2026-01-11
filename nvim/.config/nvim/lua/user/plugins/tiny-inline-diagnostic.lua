2b549c153de46c6f522d4a46a2a26c33236e79d4
Michel Garcia <michelgarxia@gmail.com>
2026-01-11 00:15:21 -0500

    feat(nvim): add tiny inline diagnostic plugin

---
+++ nvim/.config/nvim/lua/user/plugins/tiny-inline-diagnostic.lua
@@ -0,0 +1,23 @@
+return {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    config = function()
+        local diagnostic = require("tiny-inline-diagnostic")
+        diagnostic.setup({
+            options = {
+                add_messages = {
+                    use_max_severity = true,
+                },
+                enable_on_insert = true,
+                multilines = {
+                    enabled = true,
+                },
+                use_icons_from_diagnostic = true,
+            },
+            preset = "classic",
+            transparent_bg = true,
+        })
+        vim.diagnostic.config({
+            virtual_text = false,
+        })
+    end,
+}
