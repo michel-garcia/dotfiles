77e2d72bc4c7ff141334abba2f273dc82ced0c9c
Michel Garcia <michelgarxia@gmail.com>
2026-02-20 21:45:17 -0500

    feat(fish): update shell env vars

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,9 @@
 if status is-interactive
     eval "$(zoxide init --cmd cd fish)"
 end
+
+set -gx EDITOR "nvim"
+set -gx ANDROID_HOME "$HOME/Android/Sdk"
+
+fish_add_path --append "$HOME/.local/bin"
+fish_add_path --append "$HOME/.config/composer/vendor/bin"
