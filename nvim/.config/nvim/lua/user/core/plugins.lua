7b49b43b316cf8d352599ba4dc5408780132f620
Michel Garcia <michelgarxia@gmail.com>
2026-02-26 16:20:39 -0500

    feat(nvim): improve plugin manager config

--- nvim/.config/nvim/lua/user/core/plugins.lua
+++ nvim/.config/nvim/lua/user/core/plugins.lua
@@ -1,34 +1,49 @@
-local files = vim.api.nvim_get_runtime_file("lua/user/plugins/**/*.lua", true)
+local import
+
+import = function(name, res, specs)
+    local items = not vim.islist(res) and { res } or res
+    vim.iter(items):each(function(item)
+        local src = type(item) == "string" and item or item[1] or nil
+        if not src or type(src) ~= "string" then
+            local msg = table.concat({
+                string.format("Failed to load: %s", name),
+                "Missing source in spec",
+            }, "\n")
+            vim.notify(msg, vim.log.levels.ERROR)
+            return
+        end
+        import(name, item.dependencies or {}, specs)
+        table.insert(specs, {
+            src = string.format("https://github.com/%s", src),
+            data = {
+                config = item.config,
+            },
+        })
+    end)
+end
+
+local plugins_path = "user/plugins"
+local pattern = string.format("lua/%s/**/*.lua", plugins_path)
+local files = vim.api.nvim_get_runtime_file(pattern, true)
 local specs = vim.iter(files):fold({}, function(specs, path)
-    local basename = vim.fn.fnamemodify(path, ":s?.*lua/user/plugins/??:r")
-    local modname = string.format("user.plugins.%s", basename)
-    local ok, res = pcall(require, modname)
+    local name = vim.fn.fnamemodify(path, "%:t")
+    local ok, res = pcall(dofile, path)
     if not ok or type(res) ~= "table" then
         local msg = table.concat({
-            string.format("Failed to load: %s", basename),
+            string.format("Failed to load: %s", name),
             res,
         }, "\n")
         vim.notify(msg, vim.log.levels.ERROR)
         return specs
     end
-    local items = vim.islist(res) and res or { res }
-    for _, item in ipairs(items) do
-        local name = item[1] or nil
-        if name and type(name) == "string" then
-            local spec = {
-                src = table.concat({ "https://github.com/", name }, "/"),
-                data = {
-                    config = item.config,
-                },
-            }
-            table.insert(specs, spec)
-        end
-    end
+    import(name, res, specs)
     return specs
 end)
+
 vim.pack.add(specs, {
     confirm = false,
 })
+
 for _, spec in ipairs(specs) do
     if spec.data.config and vim.is_callable(spec.data.config) then
         spec.data.config()
--- nvim/.config/nvim/lua/user/plugins/fzf.lua
+++ nvim/.config/nvim/lua/user/plugins/fzf.lua
@@ -1,9 +1,9 @@
 return {
     {
-        "nvim-tree/nvim-web-devicons",
-    },
-    {
         "ibhagwan/fzf-lua",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
         config = function()
             local fzf = require("fzf-lua")
             fzf.setup({
