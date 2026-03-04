3bd7db1a17e7f1e06afe996b5a103fe71e7601f6
Michel Garcia <michelgarxia@gmail.com>
2026-03-04 16:59:08 -0500

    feat(nvim): refactor lualine config

--- nvim/.config/nvim/lua/user/plugins/lualine.lua
+++ nvim/.config/nvim/lua/user/plugins/lualine.lua
@@ -1,23 +1,74 @@
+local Diagnostics = function(config)
+    local Diagnostics = require("lualine.component.diagnostics")
+    local diagnostics = Diagnostics(config.options)
+    return diagnostics:update_status()
+end
+
+local Filename = function(config)
+    local ok, ergoterm = pcall(require, "ergoterm")
+    if ok then
+        local win = vim.api.nvim_get_current_win()
+        local term = ergoterm.find(function(term)
+            if term:get_state("window") == win then
+                return term
+            end
+        end)
+        if term then
+            return term.name
+        end
+    end
+    local Filename = require("lualine.components.filename")
+    local filename = Filename(config.options)
+    return filename:update_status()
+end
+
+local Filetype = function(config)
+    local Filetype = require("lualine.components.filetype")
+    local filetype = Filetype(config.options)
+    return filetype:update_status()
+end
+
+local Location = function()
+    local get_location = require("lualine.components.location")
+    return get_location()
+end
+
+local Mode = function()
+    local get_mode = require("lualine.components.mode")
+    return get_mode()
+end
+
+local Spacer = function()
+    return string.char(32)
+end
+
+local Tabs = function(config)
+    local Tabs = require("lualine.components.tabs")
+    local tabs = Tabs(config.options)
+    return tabs:update_status()
+end
+
+local get_sign = function(severity)
+    local config = vim.diagnostic.config()
+    if not config then
+        return
+    end
+    return table.concat({
+        config.signs.text[severity],
+        string.char(32),
+    })
+end
+
 return {
     "nvim-lualine/lualine.nvim",
     config = function()
         local lualine = require("lualine")
-        local get_sign = function(severity)
-            local config = vim.diagnostic.config()
-            if not config then
-                return
-            end
-            return table.concat({
-                config.signs.text[severity],
-                string.char(32),
-            })
-        end
         lualine.setup({
             inactive_sections = {},
             inactive_winbar = {
                 lualine_c = {
                     {
-                        "filename",
+                        Filename,
                         color = "lualine_b_insert",
                         symbols = {
                             modified = "●",
@@ -33,19 +84,21 @@ return {
             },
             sections = {
                 lualine_a = {
-                    "mode",
+                    {
+                        Mode,
+                    },
                 },
                 lualine_b = {},
                 lualine_c = {
                     {
-                        "filename",
+                        Filename,
                         path = 1,
                         symbols = {
                             modified = "●",
                         },
                     },
                     {
-                        "diagnostics",
+                        Diagnostics,
                         colored = false,
                         symbols = {
                             error = get_sign(vim.diagnostic.severity.ERROR),
@@ -57,8 +110,12 @@ return {
                     },
                 },
                 lualine_x = {
-                    "filetype",
-                    "location",
+                    {
+                        Filetype,
+                    },
+                    {
+                        Location,
+                    },
                 },
                 lualine_y = {},
                 lualine_z = {},
@@ -66,7 +123,7 @@ return {
             tabline = {
                 lualine_b = {
                     {
-                        "tabs",
+                        Tabs,
                         tabs_color = {
                             active = "@function",
                             inactive = "lualine_c_insert",
@@ -77,16 +134,14 @@ return {
             winbar = {
                 lualine_c = {
                     {
-                        "filename",
+                        Filename,
                         color = "lualine_a_insert",
                         symbols = {
                             modified = "●",
                         },
                     },
                     {
-                        function()
-                            return string.char(32)
-                        end,
+                        Spacer,
                     },
                 },
             },
