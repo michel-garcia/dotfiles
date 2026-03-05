6bc5b04b826f22f96e36e5937d83901b67588003
Michel Garcia <michelgarxia@gmail.com>
2026-03-05 10:48:58 -0500

    feat(nvim): improve terminal management

--- nvim/.config/nvim/lua/user/plugins/term.lua
+++ nvim/.config/nvim/lua/user/plugins/term.lua
@@ -1,71 +1,47 @@
+local stack = {}
+
+local spawn = function()
+    local ergoterm = require("ergoterm")
+    local terms = ergoterm.filter_by_tag("bottom")
+    for _, term in ipairs(terms) do
+        term:close()
+    end
+    local count = vim.tbl_count(terms)
+    local term = ergoterm:new({
+        border = "single",
+        name = string.format("Term %s", count + 1),
+        size = {
+            below = 12,
+        },
+        tags = { "bottom" },
+    })
+    term:focus()
+end
+
 return {
     "waiting-for-dev/ergoterm.nvim",
     config = function()
         local ergoterm = require("ergoterm")
         ergoterm.setup()
-        local pick = function()
-            ergoterm.select({
-                callbacks = {
-                    default = {
-                        desc = "Focus",
-                        fn = function(selected)
-                            local terms = ergoterm.filter_by_tag("bottom")
-                            for _, term in ipairs(terms) do
-                                if term ~= selected then
-                                    term:close()
-                                else
-                                    term:focus()
-                                end
-                            end
-                        end,
-                    },
-                    ["<C-w>"] = {
-                        desc = "Kill",
-                        fn = function(selected)
-                            selected:cleanup()
-                        end,
-                    },
-                },
-                prompt = "> ",
-            })
-        end
-        local spawn = function()
-            local terms = ergoterm.filter_by_tag("bottom")
-            for _, term in ipairs(terms) do
-                term:close()
-            end
-            local count = vim.tbl_count(terms)
-            local term = ergoterm:new({
-                border = "single",
-                name = string.format("Term %s", count + 1),
-                on_stop = function()
-                    vim.schedule(function()
-                        terms = ergoterm.filter_by_tag("bottom")
-                        for i, term in ipairs(terms) do
-                            term:update({
-                                name = string.format("Term %s", i),
-                            })
-                        end
-                    end)
-                end,
-                size = {
-                    below = 12,
-                },
-                tags = { "bottom" },
-            })
-            term:focus()
-        end
-        vim.keymap.set("n", "<leader>tt", function()
-            pick()
-        end)
         vim.keymap.set({ "n", "t" }, "<C-\\>", function()
-            local last = ergoterm.get_state("last_focused")
-            if last then
-                last:toggle()
+            local wins = vim.api.nvim_list_wins()
+            for _, win in ipairs(wins) do
+                local config = vim.api.nvim_win_get_config(win)
+                if config.relative ~= "" then
+                    return
+                end
+            end
+            local count = vim.tbl_count(stack)
+            local term = stack[count]
+            if not term then
+                spawn()
                 return
             end
-            spawn()
+            term:toggle()
         end)
+        local group = vim.api.nvim_create_augroup("ErgoTerm", {
+            clear = true,
+        })
         vim.api.nvim_create_autocmd("FileType", {
             callback = function(args)
                 local opts = {
@@ -75,20 +51,81 @@ return {
                     spawn()
                 end, opts)
                 vim.keymap.set("t", "<C-x>", function()
-                    local term = ergoterm:get_focused()
-                    if term then
-                        term:cleanup()
+                    local focused = ergoterm:get_focused()
+                    if not focused then
+                        return
                     end
+                    focused:cleanup()
+                    vim.schedule(function()
+                        local count = vim.tbl_count(stack)
+                        local term = stack[count]
+                        if term then
+                            term:focus()
+                        end
+                    end)
                 end, opts)
-                vim.keymap.set("t", "<C-o>", function()
-                    pick()
+                vim.keymap.set("t", "<C-g>", function()
+                    ergoterm.select({
+                        callbacks = {
+                            default = {
+                                desc = "Focus",
+                                fn = function(selected)
+                                    local terms = ergoterm.filter_by_tag("bottom")
+                                    for _, term in ipairs(terms) do
+                                        if term ~= selected then
+                                            term:close()
+                                        else
+                                            term:focus()
+                                        end
+                                    end
+                                end,
+                            },
+                            ["<C-w>"] = {
+                                desc = "Kill",
+                                fn = function(selected)
+                                    selected:cleanup()
+                                end,
+                            },
+                        },
+                        prompt = "> ",
+                    })
                 end, opts)
                 vim.keymap.set("t", "<C-k>", "<cmd>wincmd k<CR>", opts)
             end,
-            group = vim.api.nvim_create_augroup("TermKeys", {
-                clear = true,
-            }),
-            pattern = "ergoterm"
+            group = group,
+            pattern = "ergoterm",
+        })
+        vim.api.nvim_create_autocmd("BufEnter", {
+            callback = function(args)
+                local term = ergoterm.find(function(term)
+                    local buf = term:get_state("bufnr")
+                    local integrated = vim.tbl_contains(term.tags, "bottom")
+                    return buf == args.buf and integrated
+                end)
+                if term then
+                    local count = vim.tbl_count(stack)
+                    if stack[count] ~= term then
+                        table.insert(stack, term)
+                    end
+                end
+            end,
+            group = group,
+        })
+        vim.api.nvim_create_autocmd("WinResized", {
+            callback = function()
+                local terms = ergoterm.filter_by_tag("bottom")
+                for i, term in ipairs(terms) do
+                    term:update({
+                        name = string.format("Term %s", i),
+                    })
+                end
+                for i, term in ipairs(stack) do
+                    if not term:is_active() then
+                        table.remove(stack, i)
+                    end
+                end
+            end,
+            group = group,
         })
     end,
 }
