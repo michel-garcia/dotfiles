b1600836e58a9cdfba5b6fde4c385ecef1ba2fd2
Michel Garcia <michelgarxia@gmail.com>
2026-05-17 03:49:26 -0400

    feat(hyprland): add scratchpad rules

--- hypr/.config/hypr/lua/rules.lua
+++ hypr/.config/hypr/lua/rules.lua
@@ -1,6 +1,16 @@
+hl.workspace_rule({
+    gaps_in = 4,
+    gaps_out = 64,
+    layout = "master",
+    on_created_empty = "kitty",
+    workspace = "special:magic",
+})
+
 hl.window_rule({
     name = "suppress-maximize-events",
-    match = { class = ".*" },
+    match = {
+        class = ".*",
+    },
     suppress_event = "maximize",
 })
 
@@ -15,4 +25,12 @@ hl.window_rule({
         pin = false,
     },
     no_focus = true,
+})
+
+hl.window_rule({
+    name = "tweak-special-workspace-windows",
+    match = {
+        workspace = "special:magic",
+    },
+    no_anim = true,
 })
