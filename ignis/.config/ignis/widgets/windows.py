11439c015e432ab2947b2150a7c23f72b5eb179d
Michel Garcia <michelgarxia@gmail.com>
2026-05-21 21:59:36 -0400

    fix(ignis): correct windows order

--- ignis/.config/ignis/widgets/windows.py
+++ ignis/.config/ignis/widgets/windows.py
@@ -1,5 +1,6 @@
+import json
 from ignis.services.hyprland import HyprlandService
-from ignis.utils import get_monitor
+from ignis.utils import Poll, get_monitor
 from ignis.widgets import Widget
 
 hyprland = HyprlandService.get_default()
@@ -7,7 +8,6 @@ hyprland = HyprlandService.get_default()
 
 class Window(Widget.Button):
     def __init__(self, window):
-        self.window = window
         super().__init__(
             child=Widget.Label(
                 ellipsize="end",
@@ -15,14 +15,14 @@ class Window(Widget.Button):
                 max_width_chars=32,
             ),
             css_classes=["window"],
-            on_click=lambda _: self.focus(),
+            on_click=lambda _: self.focus(window),
         )
         if window.address == hyprland.active_window.address:
             self.add_css_class("active")
 
-    def focus(self):
+    def focus(self, window):
         command = 'dispatch hl.dsp.focus({{ window = "address:{address}" }})'.format(
-            address=self.window.address
+            address=window.address
         )
         hyprland.send_command(command)
 
@@ -31,8 +31,13 @@ class Windows(Widget.Box):
     def __init__(self, monitor):
         super().__init__(spacing=4)
         self.update(monitor)
-        for signal in ("notify::active-window", "notify::active-workspace", "notify::windows"):
+        for signal in (
+            "notify::active-window",
+            "notify::active-workspace",
+            "notify::windows",
+        ):
             hyprland.connect(signal, lambda *_: self.update(monitor))
+        Poll(100, lambda _: self.force_update(monitor))
 
     def update(self, monitor_id):
         monitor_name = get_monitor(monitor_id).get_connector()
@@ -45,3 +50,15 @@ class Windows(Widget.Box):
         ]
         windows.sort(key=lambda w: w.at[0])
         self.child = [Window(w) for w in windows]
+
+    def force_update(self, monitor_id):
+        items = json.loads(hyprland.send_command("j/clients"))
+        queue_update = False
+        for item in items:
+            address = item.get("address")
+            if address and address in hyprland._windows:
+                if hyprland._windows[address].at != item.get("at"):
+                    hyprland._windows[address].sync(item)
+                    queue_update = True
+        if queue_update:
+            self.update(monitor_id)
