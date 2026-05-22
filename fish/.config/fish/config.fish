6bd1dd9f2e8044ca41f9b7c044e8c62bc0652806
Michel Garcia <michelgarxia@gmail.com>
2026-05-21 22:28:24 -0400

    feat(fish): enable vi key bindings

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,3 +7,8 @@ set -gx ANDROID_HOME "$HOME/Android/Sdk"
 
 fish_add_path --append "$HOME/.local/bin"
 fish_add_path --append "$HOME/.config/composer/vendor/bin"
+
+fish_vi_key_bindings
+
+function fish_mode_prompt
+end
