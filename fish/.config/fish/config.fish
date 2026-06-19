d24675acc6f5b3571c34a2cb2871e6f1b8c2d398
Michel Garcia <michelgarxia@gmail.com>
2026-06-19 16:48:06 -0400

    feat(fish): add cargo bin to path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,6 +7,7 @@ set -gx ANDROID_HOME "$HOME/Android/Sdk"
 
 fish_add_path --append "$HOME/.local/bin"
 fish_add_path --append "$HOME/.config/composer/vendor/bin"
+fish_add_path --append "$HOME/.cargo/bin"
 
 fish_vi_key_bindings
 
