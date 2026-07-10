4d0819ac1afbb5eefa34934b029f777e6ff7a9be
Michel Garcia <michelgarxia@gmail.com>
2026-07-10 19:49:47 -0400

    feat(nvim): remove mason plugin

--- nvim/.config/nvim/plugin/lsp.lua
+++ nvim/.config/nvim/plugin/lsp.lua
@@ -1,7 +1,5 @@
 vim.pack.add({
     "https://github.com/neovim/nvim-lspconfig",
-    "https://github.com/mason-org/mason.nvim",
-    "https://github.com/mason-org/mason-lspconfig.nvim",
 })
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -10,25 +8,10 @@ vim.lsp.config("*", {
     capabilities = capabilities,
 })
 
-local mason = require("mason")
-mason.setup({
-    ui = {
-        border = "single",
-        icons = {
-            package_installed = "󰄬",
-            package_pending = "󰁅",
-            package_uninstalled = "󰅖",
-        },
-    },
-})
-
-local lspconfig = require("mason-lspconfig")
-lspconfig.setup({
-    ensure_installed = {
-        "clangd",
-        "intelephense",
-        "jsonls",
-        "lua_ls",
-        "ts_ls",
-    },
+vim.lsp.enable({
+    "clangd",
+    "lua_ls",
+    "pyright",
+    "phpantom_lsp",
+    "ts_ls",
 })
